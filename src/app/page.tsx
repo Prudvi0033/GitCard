88d513f8cfc873ddc6ada726ae0108605fb923da
Prudvi0033 <prudvirajvarma2004@gmail.com>
2025-07-18 20:04:33 +0530

    Added beam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,38 @@
-'use client'
-import React from 'react'
-import { Raleway } from 'next/font/google'
-import Box from './components/Box'
-import Title from './components/Title'
-import LogoBox from './components/LogoBox'
-import Tabs from './components/Tabs'
-import Pulse from './components/Pulse'
+"use client";
+import React from "react";
+import { Raleway } from "next/font/google";
+import Box from "./components/Box";
+import Title from "./components/Title";
+import LogoBox from "./components/LogoBox";
+import Tabs from "./components/Tabs";
+import Pulse from "./components/Pulse";
+import Beams from "./components/Beams";
 
-const re = Raleway({subsets: ['latin']})
-
+const re = Raleway({ subsets: ["latin"] });
 
 const page = () => {
   return (
-    <div className='flex items-center justify-center h-screen bg-[#0d0d0d] relative overflow-hidden'>
-      <Box/>
-      <div className=' absolute z-20 left-[-6.5rem] -rotate-90'>
-        <Title/>
+    <div className="flex items-center justify-center h-screen bg-[#0d0d0d] relative overflow-hidden">
+      <Box />
+      <div className=" absolute z-20 left-[-6.5rem] -rotate-90">
+        <Title />
       </div>
-      <div className='absolute top-[5.5rem] right-[12rem]'>
-        <LogoBox/>
+      <div className="absolute top-[5.5rem] right-[12rem] flex flex-row items-center gap-4">
+        <LogoBox />
       </div>
-      <div className={`absolute bottom-[1] font-semibold right-3 ${re.className}`}>
-        <Tabs/>
+      <div className=" absolute -left-12">
+        <Beams/>
+      </div>
+      <div
+        className={`absolute bottom-[1] font-semibold right-3 ${re.className}`}
+      >
+        <Tabs />
       </div>
       <div>
-        <Pulse/>
+        <Pulse />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
